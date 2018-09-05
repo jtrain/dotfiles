1fd2c57bf42e6d56dfe79d95db21858a34937fb4
Jervis Whitley <jtrain@users.noreply.github.com>
2018-09-05 03:00:52 +0000

    move specific sudo tasks into separate section

--- install.sh
+++ install.sh
@@ -4,16 +4,25 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
-            -avh --no-perms . ~;
+function watchers() {
+    # for webpack to increase watchers
+    if [ -f /etc/sysctl.conf ]; then
+        echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+    fi
+}
 
+function installVim() {
     if [ -x "$(command -v apt)" ]; then
         # install new vim
         sudo add-apt-repository ppa:jonathonf/vim
         sudo apt update
         sudo apt install vim
     fi
+}
+
+function doIt() {
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
+            -avh --no-perms . ~;
 
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
@@ -32,10 +41,6 @@ function doIt() {
 
     mkdir -p ~/bin
 
-    # for webpack to increase watchers
-    if [ -f /etc/sysctl.conf ]; then
-        echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-    fi
 }
 
 function sshKeyToGithub() {
@@ -48,19 +53,27 @@ function sshKeyToGithub() {
         https://api.github.com/user/keys
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
-    ln -s $HOME/.local/bin/* $HOME/bin/;
-    ln -s $HOME/node/bin/* $HOME/bin/;
-    sshKeyToGithub;
+if [ "$1" == "--sudo" -o "$1" == "-s" ]; then
+    installVim;
+    watchers;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-            doIt;
-            ln -s $HOME/.local/bin/* $HOME/bin/;
-            ln -s $HOME/node/bin/* $HOME/bin/;
+    if [ "$1" == "--force" -o "$1" == "-f" ]; then
+        doIt;
+        ln -s $HOME/.local/bin/* $HOME/bin/;
+        ln -s $HOME/node/bin/* $HOME/bin/;
+        sshKeyToGithub;
+    else
+        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+        echo "";
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+                doIt;
+                ln -s $HOME/.local/bin/* $HOME/bin/;
+                ln -s $HOME/node/bin/* $HOME/bin/;
+        fi;
     fi;
 fi;
+
+unset installVim;
+unset watchers;
 unset doIt;
 unset sshKeyToGithub;
